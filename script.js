38a0cda8457c9a26f27720f7f7235d9ba0293e7f
Obinna Umerah <umerahobinna@gmail.com>
2024-02-23 12:19:09 -0600

    Fixed bug causing the null error

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const container = document.querySelector(".container");
+
 
 document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.container');
     // Loop to create 256 div elements
     for (let i = 0; i < 256; i++) {
         // Create a new div element
