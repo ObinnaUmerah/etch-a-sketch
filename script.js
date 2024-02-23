86b1681d3e2043883a175084bfd095259f606b26
Obinna Umerah <umerahobinna@gmail.com>
2024-02-23 11:12:49 -0600

    Created div elements using DOM methods and also began working on adding container

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Loop to create 256 div elements
+    for (let i = 0; i < 256; i++) {
+        // Create a new div element
+        let div = document.createElement("div");
+        
+        // Append the div element to the body of the webpage
+        document.body.appendChild(div);
+        container.appendChild(div);
+        console.log(container);
+    }
+});
